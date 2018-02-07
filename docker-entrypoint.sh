c1c87f72f812d6c24e79e28370b6529176ff8414
Rafael Carreira <rafaelcarreira@macromind.com.br>
2018-02-07 11:05:55 -0200

    first commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+set -e
+
+echo "[Entrypoint] ASSP"
+
+DIR="assp"
+
+if [ "$GIT_URL" -a "$ASSP_URL" ]
+then
+	if [ -f "/root/.ssh/server-rsa" ]
+	then
+		eval `ssh-agent`
+		ssh-add /root/.ssh/server-rsa
+		ssh-keyscan bitbucket.org >> /root/.ssh/known_hosts
+		cd /usr/share
+		if [ -d "$DIR" ]
+		then
+			cd $DIR
+			git pull
+		else
+			git clone "$GIT_URL" $DIR
+			wget -O assp_new.zip $ASSP_URL
+			unzip -n assp_new.zip "assp/*"
+
+
+			if [ "$MYSQL_DATABASE" -a "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]
+			then
+				echo "not changing assp.cfg yet!..."	
+			else
+				echo "Missing db parameters!"
+				exit 1
+			fi
+
+
+			## coping assp/mysql/dbbackup to assp/mysql/dbimport
+			if [ "$MYSQL_URL" ]
+			then
+				echo "Download DB backup.."
+				cd /tmp
+				wget -O dbbackup.tar.gz $MYSQL_URL
+				mkdir -p /usr/share/assp/mysql/dbimport
+				tar -xvzf dbbackup.tar.gz -C /usr/share/assp/mysql/dbimport
+				cd /usr/share/assp/mysql/dbimport
+				for i in *
+				do
+					mv $i $i.add
+				done
+			fi	
+		fi
+
+	else
+		echo "no ssh key found in: /root/.ssh/server-rsa"
+		exit 1
+	fi
+else
+	echo "not receiving a git repo in GIT_URL or assp dl link"
+	exit 1
+
+fi
+
+# build supervisor config
+cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
+
+[supervisord]
+nodaemon=true
+
+[program:postfix]
+process_name	= master
+directory	= /etc/postfix
+command		= /usr/sbin/postfix -c /etc/postfix start
+startsecs	= 0
+autorestart = false
+
+[program:assp]
+command		= /usr/bin/perl /usr/share/assp/assp.pl
+directory	= /usr/share/assp
+
+[program:rsyslog]
+command		= /usr/sbin/rsyslogd -n
+
+[program:keep-updated]
+command		= /keep-updated.sh
+stdout_logfile	= /var/log/keep-updated.log
+
+
+EOF
+
+# Create transport mapping
+/usr/sbin/postmap /usr/share/assp/postfix/transport
+
+sleep 5
+
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf	
---
+++ keep-updated.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# Script to keep a git repository always updated
+#
+
+# Time between checks (s)
+INTERVAL=30
+
+# Path to repository 
+DIR=/usr/share/assp
+
+# Path to SSH KEY
+KEY=/root/.ssh/server-rsa
+
+
+# Function to start ssh-agent and add ssh-key
+init() {
+
+	eval `ssh-agent`
+	ssh-add $KEY
+
+}
+
+# Function to check
+check() {
+	
+	cd $DIR
+
+	while true
+	do
+		sleep $INTERVAL
+		if git pull|grep -q 'Already up-to-date.'
+		then
+			echo "[check] Already updated!"
+		else
+			echo "[check] Changes detected!"
+			/usr/sbin/postmap /usr/share/assp/postfix/transport
+			/usr/sbin/postfix reload
+		fi
+	done
+}
+
+#####
+
+init
+check
